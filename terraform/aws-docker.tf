e4443ea4c129e5c12e0aec04925660f688c58036
Paul Badcock <paul@bad.co.ck>
2017-09-12 16:40:10 -0300

    SimpleWeb site ports

--- terraform/aws-docker.tf
+++ terraform/aws-docker.tf
@@ -59,7 +59,21 @@ resource "aws_security_group" "docker" {
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
-
+  
+  ingress {
+      from_port = 8081
+      to_port = 8081
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+    
+  ingress {
+      from_port = 8082
+      to_port = 8082
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+    
   egress {
     from_port   = 0
     to_port     = 0
